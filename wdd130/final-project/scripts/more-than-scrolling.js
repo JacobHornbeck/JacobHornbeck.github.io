f8954e21a31b3fa3122bed69e81dc902b90776d1
JacobHornbeck <hor20009@byui.edu>
2020-07-11 21:53:15 -0600

    lots of changes... added 404.html

--- wdd130/final-project/scripts/more-than-scrolling.js
+++ wdd130/final-project/scripts/more-than-scrolling.js
@@ -1,10 +1,10 @@
 /** Global Helper Functions **/
-var cssRules = (document.all) ? 'rules': 'cssRules';
+var cssRules = (document.all) ? 'rules': 'cssRules'
 function changeCSSStyle(selector, cssProp, cssVal) {
-    for (let i = 0; i < document.styleSheets[ssMain][cssRules].length; i++) {
-        if (document.styleSheets[ssMain][cssRules][i].selectorText === selector) {
-            document.styleSheets[ssMain][cssRules][i].style[cssProp] = cssVal;
-            return;
+    for (let i = 0; i < document.styleSheets[0][cssRules].length; i++) {
+        if (document.styleSheets[0][cssRules][i].selectorText === selector) {
+            document.styleSheets[0][cssRules][i].style[cssProp] = cssVal
+            return
         }
     }
 }
@@ -31,13 +31,17 @@ function CloseLoader() {
     cover.style.opacity = "0"
     setTimeout(() => {
         cover.style.zIndex = "-1"
-    }, 501);
+    }, 501)
     if (window.location.pathname === "/start-learning.html") {
-        setTimeout(LoadCourses,1500+Math.random()*500,'close')
+        LoadCourses('close')
     }
 }
 
 setTimeout(CloseLoader, 2000+Math.random()*500)
+
+function OpenCourse(elem) {
+    elem.getElementsByClassName("course-title-link")[0].click()
+}
 
 function OpenLink(element) {
     event.preventDefault()
@@ -50,7 +54,7 @@ function OpenLink(element) {
 
     setTimeout(() => {
         window.open(element.href, "_self")
-    }, 503);
+    }, 503)
 }
 function OpenTxtLink(urlTxt) {
     let cover = document.getElementsByClassName("page-loader-cover")[0]
@@ -61,7 +65,7 @@ function OpenTxtLink(urlTxt) {
 
     setTimeout(() => {
         window.open(urlTxt, "_self")
-    }, 503);
+    }, 503)
 }
 
 function SubmitForm(formName) {
@@ -117,26 +121,117 @@ var courses = [
     }
     */
     {
-        'image': ['images/html.png','images/css.png'],
-        'name': 'Basic Web Development',
-        'tags': ['HTML','CSS']
-    }
+        'img': [
+            '../images/card-icons/html.png',
+            '../images/card-icons/css.png'
+        ],
+        'name': '<a class="course-title-link" onclick="OpenLink(this)" href="courses/hr82s4"><h3 class="noneT"><span class="htmlT">HTML</span> & <span class="cssT">CSS</span></h3></a>',
+        'tags': ['HTML','CSS'],
+        'rank': 15,
+        'sort-name': 'html/css'
+    },
+    {
+        'img': ['../images/card-icons/cpp.png'],
+        'name': '<a class="course-title-link" onclick="OpenLink(this)" href="courses/fu832s"><h3 class="cppT">Consider C++</h3></a>',
+        'tags': ['C++'],
+        'rank': 12,
+        'sort-name': 'cpp'
+    },
+    {
+        'img': ['../images/card-icons/javascript.png'],
+        'name': '<a class="course-title-link" onclick="OpenLink(this)" href="courses/8f4sj3"><h3 class="jsT">JavaScript</h3></a>',
+        'tags': ['JavaScript'],
+        'rank': 13,
+        'sort-name': 'javascript'
+    },
+    {
+        'img': ['../images/card-icons/pjs.png'],
+        'name': '<a class="course-title-link" onclick="OpenLink(this)" href="courses/p12j7s"><h3 class="pjsT">Process PJS</h3></a>',
+        'tags': ['PJS'],
+        'rank': 8,
+        'sort-name': 'pjs'
+    },
+    {
+        'img': ['../images/card-icons/java.png'],
+        'name': '<a class="course-title-link" onclick="OpenLink(this)" href="courses/1a3va"><h3 class="javaT">Master Java</h3></a>',
+        'tags': ['Java'],
+        'rank': 0,
+        'sort-name': 'java'
+    },
+    {
+        'img': ['../images/card-icons/python.png'],
+        'name': '<a class="course-title-link" onclick="OpenLink(this)" href="courses/P63tOn"><h3 class="pythonT">Practice Python</h3></a>',
+        'tags': ['Python'],
+        'rank': 0,
+        'sort-name': 'python'
+    },
+    {
+        'img': ['../images/card-icons/ruby.png'],
+        'name': '<a class="course-title-link" onclick="OpenLink(this)" href="courses/r85b3y"><h3 class="rubyT">Think Ruby</h3></a>',
+        'tags': ['Coming Soon!'],
+        'rank': 0,
+        'sort-name': 'ruby'
+    },
+    {
+        'img': ['../images/card-icons/jquery.png'],
+        'name': '<a class="course-title-link" onclick="OpenLink(this)" href="courses/j3Q12y"><h3 class="jqT">Learn jQuery</h3></a>',
+        'tags': ['Coming Soon!'],
+        'rank': 0,
+        'sort-name': 'jquery'
+    },
+    {
+        'img': ['../images/card-icons/php.png'],
+        'name': '<a class="course-title-link" onclick="OpenLink(this)" href="courses/p1Hp41"><h3 class="phpT">Discover PHP</h3></a>',
+        'tags': ['Coming Soon!'],
+        'rank': 0,
+        'sort-name': 'php'
+    },
 ]
 
 
 function LoadCourses(arg) {
     var courseLoader = document.getElementById('course-loader')
     var container = document.getElementsByClassName('outer-container')[0]
-    var courseGrid = document.getElementById('course-grid')
+    var courseGrid = document.getElementsByClassName('course-grid')[0]
+    var titleCourse = document.querySelector('.courses-offered > h2')
     switch (arg) {
         case 'close':
-            courseLoader.className = "closed"
+            courseGrid.innerHTML = ""
             setTimeout(() => {
-                container.style.top = "-999px"
-                container.style.left = "-999px"
-                courseLoader.style.top = "-999px"
-                courseLoader.style.left = "-999px"
-            }, 503);
+                for (let i = 0; i < courses.length; i++) {
+                    if (courses[i].img.length>1) {
+                        courseGrid.innerHTML += "<div onclick=\"OpenCourse(this)\"><div class=\"img-holder two\"><div class=\""+(courses[i].tags[0].toLowerCase())+"\" style=\"background-image: url("+courses[i].img[0]+");\"></div><div class=\""+(courses[i].tags[1].toLowerCase())+"\" style=\"background-image: url("+courses[i].img[1]+")\"></div></div>"+courses[i].name+"<div class=\"tags\" id=\"tags"+i+"\"></div></div>"
+                    }
+                    else {
+                        courseGrid.innerHTML += "<div onclick=\"OpenCourse(this)\"><div class=\"img-holder "+(courses[i]["sort-name"].toLowerCase())+"\"><div style=\""+((courses[i].tags[0]==='C++')?"background-color: #00A0FD; ":"")+"background-image: url("+courses[i].img[0]+")\"></div></div>"+courses[i].name+"<div class=\"tags\" id=\"tags"+i+"\"></div></div>"
+                    }
+                    for (let j = 0; j < courses[i].tags.length; j++) {
+                        let ct = courses[i].tags[j]
+                        document.getElementById("tags"+i).innerHTML += "<div class=\"tag "+((i<1)?(courses[i].tags[j].toLowerCase()):(courses[i]["sort-name"].toLowerCase()))+"\">"+courses[i].tags[j]+"</div>"
+                    }
+                }
+                titleCourse.className = 'up'
+                changeCSSStyle(
+                    ".overflow-handler",
+                    "height",
+                    "calc((100px * "+(Math.floor(courses.length/3))+") +"+
+                    " (20px * "+(Math.floor(courses.length/3)-1)+"))"
+                )
+                changeCSSStyle(
+                    ".course-grid",
+                    "grid-template-rows",
+                    "repeat("+(Math.floor(courses.length/3)+1)+",100px)"
+                )
+            }, 1500);
+            setTimeout(() => {
+                courseLoader.className = "closed"
+                setTimeout(() => {
+                    container.style.top = "-999px"
+                    container.style.left = "-999px"
+                    courseLoader.style.top = "-999px"
+                    courseLoader.style.left = "-999px"
+                }, 503)
+            }, 1700+Math.random()*400)
         break;
         case 'load':
             container.style.top = "0"
@@ -144,6 +239,13 @@ function LoadCourses(arg) {
             courseLoader.style.top = "0"
             courseLoader.style.left = "0"
             courseLoader.className = "open"
+            titleCourse.className = 'down'
+            changeCSSStyle(
+                ".overflow-handler",
+                "height",
+                "calc((100px + 20px) * 2)"
+            )
         break;
     }
 }
+
