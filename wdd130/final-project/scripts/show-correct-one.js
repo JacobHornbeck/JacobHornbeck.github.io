9d47271d66e1a7a8b9a1093758321dffadbd6fde
JacobHornbeck <hor20009@byui.edu>
2020-07-20 17:43:15 -0600

    lots of changes!

--- wdd130/final-project/scripts/show-correct-one.js
+++ wdd130/final-project/scripts/show-correct-one.js
@@ -31,3 +31,58 @@ function ShowCorrectOne(show) {
 }
 
 setTimeout(ShowCorrectOne,1000,getUrlVars()['showPage'])
+
+var checkNow = false
+var elemLater = null
+var laterElem = null
+var timeoutSe = null
+var started = false
+
+function CheckValue(elem) {
+    if (event.keyCode === 8) {
+        LoadPerCharacter(true,elem)
+    }
+}
+
+function LoadPerCharacter(tr,elem) {
+    if (elem) {
+        started = true
+        elemLater = elem.parentElement.getElementsByClassName('valid')[0]
+        elemLater.className = 'valid loading'
+        elemLater.title = 'Checking username'
+        laterElem = elem
+    }
+    else {
+        elemLater.className = 'valid loading'
+        elemLater.title = 'Checking username'
+    }
+    if (checkNow) {
+        if (laterElem.value.length > 0) {
+            elemLater.className = 'valid true'
+            elemLater.title = 'That username is available'
+
+            if (laterElem.id == 'password-confirm') {
+                let oPass = document.getElementById('password-create')
+                if (oPass.value == laterElem.value) {
+                    elemLater.className = 'valid true'
+                }
+                else {
+                    elemLater.className = 'valid false'
+                }
+            }
+        }
+        else {
+            elemLater.className = 'valid false'
+            elemLater.title = 'That username is taken... choose another'
+        }
+        checkNow = false
+    }
+    if (tr) {
+        clearTimeout(timeoutSe)
+        timeoutSe = setTimeout(() => {
+            checkNow = true
+            LoadPerCharacter(false)
+        }, 900)
+    }
+}
+
