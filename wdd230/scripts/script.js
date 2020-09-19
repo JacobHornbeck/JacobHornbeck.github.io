90f79403ee1f26581ab810d3e5a8aa4089af4776
JacobHornbeck <hor20009@byui.edu>
2020-09-18 19:22:37 -0600

    fix some mroe

--- wdd230/scripts/script.js
+++ wdd230/scripts/script.js
@@ -1,15 +1,16 @@
-// Set the year for copyright
-document.getElementById("year").innerHTML = new Date().getFullYear()
+function LoadStuff() {
+    // Set the year for copyright
+    document.getElementById("year").innerHTML = new Date().getFullYear()
 
-// Set the date when last updated
-var temp = document.lastModified
-var n = parseInt(temp.substring(temp.length-8,temp.length-6))
-if (n>12) {
-    document.getElementById("updated").innerHTML = "Last Updated: "+
-        temp.substring(0,temp.length-8)+(n-12)+temp.substring(temp.length-6)+" pm"
+    // Set the date when last updated
+    var temp = document.lastModified
+    var n = parseInt(temp.substring(temp.length-8,temp.length-6))
+    if (n>12) {
+        document.getElementById("updated").innerHTML = "Last Updated: "+
+            temp.substring(0,temp.length-8)+(n-12)+temp.substring(temp.length-6)+" pm"
+    }
+    else {
+        document.getElementById("updated").innerHTML = "Last Updated: "+
+            temp+" am"
+    }
 }
-else {
-    document.getElementById("updated").innerHTML = "Last Updated: "+
-        temp+" am"
-}
-
