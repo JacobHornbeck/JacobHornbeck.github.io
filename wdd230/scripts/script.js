9e7432f5951c7db3b9a0e35d4ab3a90c9087d04f
JacobHornbeck <hor20009@byui.edu>
2020-09-18 18:21:35 -0600

    edit wdd230 stuff

--- wdd230/scripts/script.js
+++ wdd230/scripts/script.js
@@ -1,1 +1,24 @@
-document.getElementById("year").innerHTML = new Date().getFullYear();
+document.getElementById("year").innerHTML = new Date().getFullYear()
+document.getElementById("updated").innerHTML = "Last Updated: Fri Sep 18 2020 18:04:06"
+
+var sequence = ""
+var timeOutOn = false
+
+function PrintDate() {
+    var ky = event.keyCode
+    sequence+=ky.toString()
+    if (sequence=="818769828489") {
+        console.log(new Date())
+    }
+    if (timeOutOn) {
+        clearTimeout(timeout1)
+        timeOutOn = false
+    }
+    else {
+        timeOutOn = true
+        var timeout1 = setTimeout(() => {
+            sequence = ""
+            timeOutOn = false
+        }, 2000)
+    }
+}
