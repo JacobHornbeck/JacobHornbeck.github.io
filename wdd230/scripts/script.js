5314851486f9347fd5d2ee28dee98e78aa1e395d
JacobHornbeck <hor20009@byui.edu>
2020-09-18 18:35:48 -0600

    fix the updated time and easy updater

--- wdd230/scripts/script.js
+++ wdd230/scripts/script.js
@@ -1,6 +1,13 @@
-document.getElementById("year").innerHTML = new Date().getFullYear()
-document.getElementById("updated").innerHTML = "Last Updated: Fri Sep 18 2020 18:04:06"
+// This just makes it easier for me to update this...
+var updateWhen = "Fri Sep 18 2020 6:33:31 PM"
 
+// Set the year for copyright
+document.getElementById("year").innerHTML = new Date().getFullYear()
+
+// Set the date when last updated
+document.getElementById("updated").innerHTML = "Last Updated: "+updateWhen
+
+// Stuff for printing the date when I need it. Lines (11 - 39)
 var sequence = ""
 var timeOutOn = false
 
@@ -8,7 +15,15 @@ function PrintDate() {
     var ky = event.keyCode
     sequence+=ky.toString()
     if (sequence=="818769828489") {
-        console.log(new Date())
+        let temp = new Date().toString()
+        let t1 = temp.substring(0,temp.indexOf("GMT")-1)
+        var n1 = parseInt(t1.substring(t1.length-8,t1.length-6))
+        if (n1 > 12) {
+            console.log(t1.substring(0,t1.length-8)+(n1-12)+t1.substring(t1.length-6,t1.length)+" PM")
+        }
+        else {
+            console.log(t1+" AM")
+        }
     }
     if (timeOutOn) {
         clearTimeout(timeout1)
