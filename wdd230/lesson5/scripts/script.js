bf2e9cee0a948c9c157f3001b4e93cc8af828af2
JacobHornbeck <hor20009@byui.edu>
2020-10-14 16:01:53 -0600

    move the icons where I need them and add some files

---
+++ wdd230/lesson5/scripts/script.js
@@ -0,0 +1,12 @@
+document.getElementsByClassName('last-updated')[0].innerHTML = "Last Updated: "+document.lastModified
+
+function toggleMenu() {
+    var temp = document.getElementsByClassName("toggler")[0]
+    temp.classList.toggle("hide")
+    if (temp.classList[1]=="hide") {
+        document.querySelector(".toggler > a").innerHTML = "&#9776; Menu"
+    }
+    else {
+        document.querySelector(".toggler > a").innerHTML = "&#10006; Close"
+    }
+}
