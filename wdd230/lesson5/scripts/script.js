aa1857011341296cae704bbcc0866f20c98cc03f
JacobHornbeck <hor20009@byui.edu>
2020-10-16 22:11:47 -0600

    stuff

--- wdd230/lesson5/scripts/script.js
+++ wdd230/lesson5/scripts/script.js
@@ -11,8 +11,27 @@ function toggleMenu() {
     }
 }
 
-var dayOfWeek = new Date().getDay() // This gives us a number (1-7)
-
-if (dayOfWeek == 5) {
+if (new Date().getDay() == 5) {
     document.getElementById("big-banner").style.display = "block"
 }
+
+function closeBanner() {
+    document.getElementById("big-banner").style.display = "none"
+}
+
+var data = null;
+
+var xhr = new XMLHttpRequest();
+xhr.withCredentials = true;
+
+xhr.addEventListener("readystatechange", function () {
+	if (this.readyState === this.DONE) {
+		console.log(this.responseText);
+	}
+});
+
+xhr.open("GET", "https://weatherbit-v1-mashape.p.rapidapi.com/forecast/3hourly?units=I&lang=en&lat=42.1&lon=-111.88");
+xhr.setRequestHeader("x-rapidapi-host", "weatherbit-v1-mashape.p.rapidapi.com");
+xhr.setRequestHeader("x-rapidapi-key", "243fe7e7d2msh1d1778fe11aeee4p16940ejsn888f93619ce4");
+
+xhr.send(data);
