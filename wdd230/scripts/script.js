44f98dd1e493d7d83758b52b1cdf080bf0b572fc
JacobHornbeck <hor20009@byui.edu>
2020-10-19 22:56:33 -0600

    edit stuff

--- wdd230/scripts/script.js
+++ wdd230/scripts/script.js
@@ -1,16 +1,23 @@
-function LoadStuff() {
-    // Set the year for copyright
-    document.getElementById("year").innerHTML = new Date().getFullYear()
+WebFont.load({
+    google: {
+        families: [
+            'Gugi'
+        ]
+    }
+})
 
-    // Set the date when last updated
-    var temp = document.lastModified
-    var n = parseInt(temp.substring(temp.length-8,temp.length-6))
-    if (n>12) {
-        document.getElementById("updated").innerHTML = "Last Updated: "+
-            temp.substring(0,temp.length-8)+(n-12)+temp.substring(temp.length-6)+" pm"
-    }
-    else {
-        document.getElementById("updated").innerHTML = "Last Updated: "+
-            temp+" am"
-    }
+
+// Set the year for copyright
+document.getElementById("year").innerHTML = new Date().getFullYear()
+
+// Set the date when last updated
+var temp = document.lastModified
+var n = parseInt(temp.substring(temp.length-8,temp.length-6))
+if (n>12) {
+    document.getElementById("updated").innerHTML = "Last Updated: "+
+        temp.substring(0,temp.length-8)+(n-12)+temp.substring(temp.length-6)+" pm"
+}
+else {
+    document.getElementById("updated").innerHTML = "Last Updated: "+
+        temp+" am"
 }
