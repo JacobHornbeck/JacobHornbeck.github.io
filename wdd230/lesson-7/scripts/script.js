a8cb4fcd6dd12114086a6e7a6fd1d85292138fae
JacobHornbeck <hor20009@byui.edu>
2020-10-28 14:43:13 -0600

    started and finished lazy-loading page

---
+++ wdd230/lesson-7/scripts/script.js
@@ -0,0 +1,34 @@
+const allImages = document.querySelectorAll("img[data-src]")
+
+const lazyLoad = (img) => {
+    img.setAttribute("src", img.getAttribute("data-src"))
+    img.onload = () => {
+        img.removeAttribute("data-src")
+        img.className = "in"
+    };
+};
+
+
+const options = {
+    threshold: 1,
+    rootMargin: "0px 0px 50px 0px"
+}
+
+if ('IntersectionObserver' in window) {
+    const obsrvr = new IntersectionObserver((items, observer) => {
+        items.forEach((item) => {
+            if(item.isIntersecting) {
+                lazyLoad(item.target)
+                observer.unobserve(item.target)
+            }
+        })
+    }, options)
+    allImages.forEach((img) => {
+        obsrvr.observe(img)
+    })
+}
+else {
+    allImages.forEach((img) => {
+        lazyLoad(img)
+    })
+}
