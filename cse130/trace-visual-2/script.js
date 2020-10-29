f2e87df711b5739ab077cc16d28e1a7a565c6f13
JacobHornbeck <hor20009@byui.edu>
2020-10-28 20:47:02 -0600

    fix some things

--- cse130/trace-visual-2/script.js
+++ cse130/trace-visual-2/script.js
@@ -8,21 +8,21 @@ const code = [
 ]
 const cnt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 const sequence = [
-    ["A","[\"snake\": 0, ...]","/",        "/","/"," (1)"],
-    ["B","[\"snake\": 0, ...]",  0,        "/","/"," (2)"],
-    ["C","[\"snake\": 0, ...]",  0,    "snake",  0," (3)"],
-    ["D","[\"snake\": 0, ...]",  0,    "snake",  0," (4)"],
-    ["C","[\"snake\": 0, ...]",  0,    "snail",  1," (3)"],
-    ["D","[\"snake\": 0, ...]",  1,    "snail",  1," (4)"],
-    ["C","[\"snake\": 0, ...]",  1,   "person",  2," (3)"],
-    ["D","[\"snake\": 0, ...]",  3,   "person",  2," (4)"],
-    ["C","[\"snake\": 0, ...]",  3,      "dog",  4," (3)"],
-    ["D","[\"snake\": 0, ...]",  7,      "dog",  4," (4)"],
-    ["C","[\"snake\": 0, ...]",  7,"butterfly",  6," (3)"],
-    ["D","[\"snake\": 0, ...]", 13,"butterfly",  6," (4)"],
-    ["C","[\"snake\": 0, ...]", 13,     "crab",  8," (3)"],
-    ["D","[\"snake\": 0, ...]", 21,     "crab",  8," (4)"],
-    ["E","[\"snake\": 0, ...]", 21,     "crab",  8," (5)"],
+    ["A","[\"snake\": 0, ...]","/",        "/","/", 1],
+    ["B","[\"snake\": 0, ...]",  0,        "/","/", 2],
+    ["C","[\"snake\": 0, ...]",  0,    "snake",  0, 3],
+    ["D","[\"snake\": 0, ...]",  0,    "snake",  0, 4],
+    ["C","[\"snake\": 0, ...]",  0,    "snail",  1, 3],
+    ["D","[\"snake\": 0, ...]",  1,    "snail",  1, 4],
+    ["C","[\"snake\": 0, ...]",  1,   "person",  2, 3],
+    ["D","[\"snake\": 0, ...]",  3,   "person",  2, 4],
+    ["C","[\"snake\": 0, ...]",  3,      "dog",  4, 3],
+    ["D","[\"snake\": 0, ...]",  7,      "dog",  4, 4],
+    ["C","[\"snake\": 0, ...]",  7,"butterfly",  6, 3],
+    ["D","[\"snake\": 0, ...]", 13,"butterfly",  6, 4],
+    ["C","[\"snake\": 0, ...]", 13,     "crab",  8, 3],
+    ["D","[\"snake\": 0, ...]", 21,     "crab",  8, 4],
+    ["E","[\"snake\": 0, ...]", 21,     "crab",  8, 6],
 ]
 
 code.forEach((lineOfCode,index) => {
@@ -33,16 +33,16 @@ code.forEach((lineOfCode,index) => {
 function animateTrace(n) {
     if (n <= sequence.length) {
         if (n < sequence.length) {
-            document.querySelector('.code-line:nth-child('+(cnt.indexOf(sequence[n][0])+1)+')').classList.toggle('active')
+            document.querySelector('.code-line:nth-child('+sequence[n][5]+')').classList.toggle('active')
             document.querySelector('.trace-table tbody').innerHTML
-                += "<th>"+sequence[n][0]+sequence[n][5]+"</th>"+
+                += "<th>"+sequence[n][0]+" ("+sequence[n][5]+")</th>"+
                    "<td>"+sequence[n][1]+"</td>"+
                    "<td>"+sequence[n][2]+"</td>"+
                    "<td>"+sequence[n][3]+"</td>"+
                    "<td>"+sequence[n][4]+"</td>"
         }
         if (n > 0) {
-            document.querySelector('.code-line:nth-child('+(cnt.indexOf(sequence[n-1][0])+1)+')').classList.toggle('active')
+            document.querySelector('.code-line:nth-child('+sequence[n-1][5]+')').classList.toggle('active')
         }
         setTimeout(() => {
             animateTrace(n+1)
