f9f1974ab95237a41d90b0c3d6629e032235856b
JacobHornbeck <hor20009@byui.edu>
2020-11-06 15:30:48 -0700

    just some notes

--- wdd230/student-presentation/scripts/ls-1.js
+++ wdd230/student-presentation/scripts/ls-1.js
@@ -1,7 +1,10 @@
 function storeVal() {
+    /*  Use localStorage.setItem() to store the value in the input. Remember
+        that localStorage.setItem() is best to use, but there are other methods  */
     localStorage.setItem('ls_1_message', document.getElementById('local-storage-set').value)
 }
 
 function getVal() {
+    /*  Use localStorage.getItem() to retrieve the value from localStorage */
     document.getElementById('local-storage-get').value = localStorage.getItem('ls_1_message')
 }
