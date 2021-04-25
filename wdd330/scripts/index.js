4a3b139348526c989dfac4df2c3f9db23f1bb318
JacobHornbeck <hor20009@byui.edu>
2021-04-24 18:57:32 -0600

    fix image src

--- wdd330/scripts/index.js
+++ wdd330/scripts/index.js
@@ -3,7 +3,7 @@ const weekDone = 1
 for (let i = 1; i <= weekDone; i++) {
     document.querySelector('#week-list').innerHTML += `<section class="weeks">
         <h2>Week ${i}</h2>
-        <a href="week${i}/notes/"><img src="../images/notes.png">Notes</a>
-        <a href="week${i}/code-examples/"><img src="../images/codes.png">Code Examples</a>
+        <a href="week${i}/notes/"><img src="images/notes.png">Notes</a>
+        <a href="week${i}/code-examples/"><img src="images/codes.png">Code Examples</a>
     </section>`
 }
