3763aab5916ce72bc9b786f69cfccc3a1b55fc9e
JacobHornbeck <hor20009@byui.edu>
2021-04-24 18:47:37 -0600

    Build WDD330 protfolio home page

---
+++ wdd330/scripts/index.js
@@ -0,0 +1,9 @@
+const weekDone = 1
+
+for (let i = 1; i <= weekDone; i++) {
+    document.querySelector('#week-list').innerHTML += `<section class="weeks">
+        <h2>Week ${i}</h2>
+        <a href="week${i}/notes/"><img src="../images/notes.png">Notes</a>
+        <a href="week${i}/code-examples/"><img src="../images/codes.png">Code Examples</a>
+    </section>`
+}
