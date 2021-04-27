06f32bca555c40b5992b1f02762ac77eca3fcd15
JacobHornbeck <hor20009@byui.edu>
2021-04-27 14:02:45 -0600

    create a link for team activities for each week

--- wdd330/scripts/index.js
+++ wdd330/scripts/index.js
@@ -1,9 +1,12 @@
-const weekDone = 1
+const weekOn = 2
 
-for (let i = 1; i <= weekDone; i++) {
+for (let i = 1; i <= weekOn; i++) {
+    if (i > 1)
+        document.querySelector('#week-list').innerHTML += "<hr>"
     document.querySelector('#week-list').innerHTML += `<section class="weeks">
         <h2>Week ${i}</h2>
         <a href="week${i}/notes/"><img src="images/notes.png">Notes</a>
         <a href="week${i}/code-examples/"><img src="images/codes.png">Code Examples</a>
+        <a href="week${i}/team-activity/"><img src="images/team.png">Team Activity</a>
     </section>`
 }
