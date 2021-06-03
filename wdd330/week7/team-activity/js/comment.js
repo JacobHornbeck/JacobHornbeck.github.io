050c7418c904c6541746177e6342c884a9234315
JacobHornbeck <hor20009@byui.edu>
2021-06-03 14:00:24 -0600

    Team activity

---
+++ wdd330/week7/team-activity/js/comment.js
@@ -0,0 +1,8 @@
+export class Comment {
+    constructor(name, date, content, hikeId) {
+        this.name = name;
+        this.date = date;
+        this.content = content;
+        this.hikeId = hikeId;
+    }
+}
---
+++ wdd330/week7/team-activity/js/hike.js
@@ -0,0 +1,12 @@
+export class Hike {
+    constructor(id, name, imgSrc, imgAlt, distance, difficulty, description, directions) {
+        this.id = id;
+        this.name = name;
+        this.imgSrc = imgSrc;
+        this.imgAlt = imgAlt;
+        this.distance = distance;
+        this.difficulty = difficulty;
+        this.description = description;
+        this.directions = directions;
+    }
+}
