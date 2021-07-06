f012eb217b5c47aa1fa8d7bf36cc2b44f548b3dd
JacobHornbeck <hor20009@byui.edu>
2021-07-06 10:32:12 -0600

    Adding styles and scripts to show the block challenge links

--- wdd330/scripts/index.js
+++ wdd330/scripts/index.js
@@ -18,12 +18,23 @@ const numbers = [
 ]
 
 for (let i = 1; i <= weekOn; i++) {
+    const weekList = document.querySelector('#week-list')
+
     if (i > 1)
-        document.querySelector('#week-list').innerHTML += "<hr>"
-    document.querySelector('#week-list').innerHTML += `<section class="weeks">
+        weekList.innerHTML += "<hr>"
+    
+    weekList.innerHTML += `<section class="weeks">
         <h2>Week ${i}</h2>
         <a href="week${i}/notes/"><img src="images/notes.png">Notes<span ${numbers[i][0] > 0 ? "" : "class=\"showOnHover\""}>${numbers[i][0]}</span></a>
         <a href="week${i}/code-examples/"><img src="images/codes.png">Code Examples<span ${numbers[i][1] > 0 ? "" : "class=\"showOnHover\""}>${numbers[i][1]}</span></a>
         <a href="week${i}/team-activity/"><img src="images/team.png">Team Activity</a>
     </section>`
+
+    if (i === 6) {
+        weekList.innerHTML += `<hr>
+            <section class="weeks">
+                <h2 class="special">Midterm Check-in</h2>
+                <a href="block1-challenge/"><img src="images/challenge.png">Block 1 Challenge</a>
+            </section>`
+    }
 }
